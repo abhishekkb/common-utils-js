7de9781b7eab41e0619256ee91f6816d86f2188f
abhishek kakkerla <abhishek.kakkerla@gmail.com>
2023-12-28 10:43:39 -0600

    simplified, handling arrays too

--- src/json/flatten-keys.js
+++ src/json/flatten-keys.js
@@ -1,41 +1,63 @@
-function flattenKeys(obj, parentKey = '', separator = '.') {
+function flattenKeys(obj, separator = '.') {
     let result = {};
 
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
+    const processObject = (currentObj, parentKey = '') => {
+        Object.keys(currentObj).forEach(key => {
             const currentKey = parentKey ? `${parentKey}${separator}${key}` : key;
 
-            if (typeof obj[key] === 'object' && obj[key] !== null) {
-                const nestedKeys = flattenKeys(obj[key], currentKey, separator);
-                result = { ...result, ...nestedKeys };
+            if (Array.isArray(currentObj[key])) {
+                currentObj[key].forEach((item, index) => {
+                    const arrayKey = `${currentKey}${separator}${index}`;
+                    if (typeof item === 'object' && item !== null) {
+                        processObject(item, arrayKey);
+                    } else {
+                        result[arrayKey] = item;
+                    }
+                });
+            } else if (typeof currentObj[key] === 'object' && currentObj[key] !== null) {
+                processObject(currentObj[key], currentKey);
             } else {
-                result[currentKey] = obj[key];
+                result[currentKey] = currentObj[key];
             }
-        }
-    }
+        });
+    };
+
+    processObject(obj);
 
     return result;
 }
 
 // Example usage:
-const nestedJsonData = {
+const nestedJsonDataWithArrays = {
     user: {
         name: 'John',
-        address: {
-            city: 'New York',
-            zip: '10001',
-        },
+        addresses: [
+            {
+                city: 'New York',
+                zip: '10001',
+            },
+            {
+                city: 'San Francisco',
+                zip: '94105',
+            },
+        ],
     },
     age: 25,
     preferences: {
-        music: {
-            genre: 'Rock',
-            instrument: 'Guitar',
-        },
+        music: [
+            {
+                genre: 'Rock',
+                instrument: 'Guitar',
+            },
+            {
+                genre: 'Pop',
+                instrument: 'Piano',
+            },
+        ],
         color: 'Blue',
     },
 };
 
-const flattenedKeys = flattenKeys(nestedJsonData);
+const flattenedKeysWithArrays = flattenKeys(nestedJsonDataWithArrays);
 
-console.log(flattenedKeys);
+console.log(flattenedKeysWithArrays);
